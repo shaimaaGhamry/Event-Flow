bae6a29c4658e9b546a6935b8ad63a4be4faaf5e
Marie-dale Pais <mdpais@gmail.com>
2023-04-13 17:36:37 -0300

    Added Attending section to MyEvents and colour coded based on event scope

--- client/src/routes/MyEvents.js
+++ client/src/routes/MyEvents.js
@@ -7,11 +7,25 @@ const MyEvents = () => {
     { title: "GHI", type: "Trip", owner: "Colin Creevey", sdate: "28 Apr 2023", edate: "5 May 2023" },
     { title: "JKL", type: "Birthday", owner: "Draco Malfoy", sdate: "5 May 2023", edate: "5 May 2023" },
   ];
+  const attendingEvents = [
+    { title: "MNO", type: "Baby Shower", owner: "Albus Dumbeldore", sdate: "15 Apr 2023", edate: "15 Apr 2023", scope: "public" },
+    { title: "PQR", type: "Graduation", owner: "Beatrix Lestrange", sdate: "20 Apr 2023", edate: "20 Apr 2023", scope: "private" },
+    { title: "STU", type: "Trip", owner: "Colin Creevey", sdate: "28 Apr 2023", edate: "5 May 2023", scope: "private" },
+    { title: "VWX", type: "Birthday", owner: "Draco Malfoy", sdate: "5 May 2023", edate: "5 May 2023", scope: "public" },
+  ];
+  const expiredEvents = [
+    { title: "YZ", type: "Baby Shower", owner: "Albus Dumbeldore", sdate: "15 Apr 2023", edate: "15 Apr 2023", scope: "public" },
+  ];
   return (
     <div className="main">
-      <div class="row">
+      <div class="row pending">
         <div class="event-list"><p><span class="event-list">PENDING INVITES</span></p></div>
         {pendingInvites.map((app) => <App type={app.type} title={app.title} owner={app.owner} sdate={app.sdate} edate={app.edate} />)}
+      </div>
+      <div class="row attending">
+        <div class="event-list"><p><span class="event-list">ATTENDING EVENTS</span></p></div>
+        {attendingEvents.map((app) => <App type={app.type} title={app.title} owner={app.owner} sdate={app.sdate} edate={app.edate} scope={app.scope} />)}
+        {expiredEvents.map((app) => <App type={app.type} title={app.title} owner={app.owner} sdate={app.sdate} edate={app.edate} scope="expired" />)}
       </div>
     </div>
   );
@@ -20,19 +34,21 @@ const MyEvents = () => {
 function App(props) {
   return (
     <div class="card">
-      <div class="card-content">
-        <div class="media">
-          {/*       === For event type image ===   
+      <div class={props.scope}>
+        <div class="card-content">
+          <div class="media">
+            {/*       === For event type image ===   
           <div class="media-left">
             <figure class="image is-48x48">
               <img src="img-link" alt="Placeholder image"></img>
             </figure>
           </div> */}
-          <div class="media-content">
-            <p class="title is-4">{props.title}</p>
-            <p class="subtitle is-6">({props.type})</p>
-            <p class="subtitle is-6">Event Owner: <b>{props.owner}</b></p>
-            <p class="subtitle is-6"> Starts on <b>{props.sdate}</b> and ends on <b>{props.edate}</b></p>
+            <div class="media-content">
+              <p class="title is-4">{props.title}</p>
+              <p class="subtitle is-6">({props.type})</p>
+              <p class="subtitle is-6">Event Owner: <b>{props.owner}</b></p>
+              <p class="subtitle is-6"> Starts on <b>{props.sdate}</b> and ends on <b>{props.edate}</b></p>
+            </div>
           </div>
         </div>
       </div>
