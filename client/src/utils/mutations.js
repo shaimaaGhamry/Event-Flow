0e876314b8866a3f5127d48a98de7f06274f30d9
unknown <shimaa_elghamry@hotmail.com>
2023-04-14 19:33:06 -0300

    change the user mutation to return auth not user and test the adduser and login mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        email
+        id
+        userName
+      }
+    }
+  }
+  `;
+
+  export const ADD_USER = gql`
+   mutation addUser($userName: String!, $email: String!, $password: String!) {
+    addUser(userName: $userName, email: $email, password: $password) {
+      token
+      user {
+        email
+        id
+        userName
+      }
+    }
+  }
+  `;
