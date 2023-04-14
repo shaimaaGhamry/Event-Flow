e93b9a07ee879633f0d13a6dc51c6ad8b71bcd9d
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-14 10:20:28 -0300

    updated login.js to interact with user

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,29 +2,39 @@ import React from 'react';
 
 const LoginForm = () => {
     return (
-        <div class="hero is-fullheight">
-            <div class="hero-body is-justify-content-center is-align-items-center">
-                <div class="columns is-flex is-flex-direction-column box">
-                    <div class="column">
-                        <label for="email">Email</label>
-                        <input class="input is-primary" type="text" placeholder="Email address"></input>
+        <section class="hero  is-fullheight is-fullwidth">
+        <div class="hero-body is-justify-content-center is-align-items-center">
+            <div class="columns is-flex is-flex-direction-column box">
+                <form action="" class="box">
+                    <div class="field">
+                        <label for="" class="label">Email</label>
+                        <input type="email" placeholder="e.g. bobsmith@gmail.com" class="input is-primary" required></input>
                     </div>
-                    <div class="column">
-                        <label for="Name">Password</label>
-                        <input class="input is-primary" type="password" placeholder="Password"></input>
-                            <a href="forget.html" class="is-size-7 has-text-primary">forget password?</a>
+                    <div class="field">
+                        <label for="" class="label">Password</label>
+                        <input type="password" placeholder="*******" class="input is-primary" required></input>
                     </div>
-                    <div class="column">
-                        <button class="button is-primary is-fullwidth" type="submit">Login</button>
+                    <div class="field">
+                        <label for="" class="checkbox">
+                            <input type="checkbox"></input>
+                            Remember me
+                        </label>
+                    </div>
+                    <div class="field">
+                        <button class="button is-success is-primary is-fullwidth" type="submit">
+                            Login
+                        </button>
                     </div>
                     <div class="has-text-centered">
-                        <p class="is-size-7"> Don't have an account? <a href='/SignUp' class="has-text-primary">Sign up</a>
+                        <p class="is-size-7"> Don't have an account? <a href='./SignUp' class="has-text-primary">Sign up</a>
                         </p>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
-                    )
+    </section>
+)
+                    
 };
 
 
