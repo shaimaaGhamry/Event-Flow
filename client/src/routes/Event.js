56ec679b94695819e76fcef6ef2b45728883c8f4
Marie-dale Pais <mdpais@gmail.com>
2023-04-15 17:19:17 -0300

    formatted pages and created Event Details template page

--- client/src/routes/Event.js
+++ client/src/routes/Event.js
@@ -2,10 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Event = () => {
-  
+
+  const isPublic = false;
+
   return (
-    <div className="main">
-      Event Details
+    <div className="main events-main">
+      <div className="events">
+        <div className="title is-4">Events that you might be interested in:</div>
+        <div className="row">
+          Event details here
+        </div>
+      </div>
+      <div className="actions">
+        {isPublic
+          ? ""
+          : <div>
+            <div className="attendees">
+              Attendees
+            </div>
+            <div className="attendees">
+              Tasks
+            </div>
+          </div>
+        }
+      </div>
     </div>
   );
 }
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -11,17 +11,17 @@ const Home = () => {
   return (
     <div className="main events-main">
       <div className="events">
-      <div className="title is-4">Events that you might be interested in:</div>
+        <div className="title is-4">Events that you might be interested in:</div>
         <div className="row">
           {publicEvents.map((app) => <App type={app.type} title={app.title} owner={app.owner} sdate={app.sdate} edate={app.edate} id={app.id} scope="public" />)}
         </div>
       </div>
       <div className="actions">
-          <Link to="/CreateEvent" className="div-button">
-        <div className="create-button">
+        <Link to="/CreateEvent" className="div-button">
+          <div className="create-button">
             <p>Create</p>
             <p>New Event</p>
-        </div>
+          </div>
         </Link>
       </div>
     </div>
@@ -32,24 +32,24 @@ function App(props) {
   return (
     <div className="card">
       <Link to={`./event/${props.id}`}>
-      <div className={props.scope}>
-        <div className="card-content">
-          <div className="media">
-            {/*       === For event type image ===   
+        <div className={props.scope}>
+          <div className="card-content">
+            <div className="media">
+              {/*       === For event type image ===   
           <div className="media-left">
             <figure className="image is-48x48">
               <img src="img-link" alt="Placeholder image"></img>
             </figure>
           </div> */}
-            <div className="media-content">
-              <p className="title is-4">{props.title}</p>
-              <p className="subtitle is-6">({props.type})</p>
-              <p className="subtitle is-6">Event Owner: <b>{props.owner}</b></p>
-              <p className="subtitle is-6"> Starts on <b>{props.sdate}</b> and ends on <b>{props.edate}</b></p>
+              <div className="media-content">
+                <p className="title is-4">{props.title}</p>
+                <p className="subtitle is-6">({props.type})</p>
+                <p className="subtitle is-6">Event Owner: <b>{props.owner}</b></p>
+                <p className="subtitle is-6"> Starts on <b>{props.sdate}</b> and ends on <b>{props.edate}</b></p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </Link>
     </div>
   )
