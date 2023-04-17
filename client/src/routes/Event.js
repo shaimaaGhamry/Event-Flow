29a581fe082f16208196c6a49a8c57675ee42717
Marie-dale Pais <mdpais@gmail.com>
2023-04-16 23:57:50 -0300

    formatted event details page

--- client/src/routes/Event.js
+++ client/src/routes/Event.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { EVENT_BY_ID } from '../utils/queries';
 
 const Event = () => {
-  
-  const isPublic = false;
 
   const { eventId } = useParams();
   const { loading, data } = useQuery(EVENT_BY_ID, {
@@ -19,21 +17,43 @@ const Event = () => {
   return (
     <div className="main events-main">
       <div className="events">
-        <div className="title is-4">{event.name} by {event.createdBy.userName}</div>
-        <div className="row">
-          <p>Type: {event.type}</p>
-          <p>Starting on {event.startDate} and ending on {event.endDate}</p>
-          <p>Location:{event.location}</p>
-          <p>Details:</p>
-          <p>{event.description}</p>
-        </div>
+        <article className={event.isPrivate ? "message private-body" : "message public-body"}>
+          <div className={event.isPrivate ? "message-header private title is-5" : "message-header public title is-5"}>
+            <p>{event.name} by {event.createdBy.userName}</p>
+          </div>
+          <div className="message-body">
+            <p>Type: <b>{event.type}</b></p>
+            <p>Starts on <b>{event.startDate}</b> and ends on <b>{event.endDate}</b></p>
+            <p>Location: <b>{event.location}</b></p>
+            <p>Created by: <b>{event.createdBy.userName}</b></p>
+            <p>This is a <b>{event.isPrivate ? "private" : "public"}</b> event</p>
+            <p>Details:</p>
+            <p className="details">{event.description}</p>
+          </div>
+          <footer class="card-footer">
+            <div className="buttons">
+              {event.isPrivate?"":
+              <Link to="">
+                <a className="button is-primary">Join</a>
+              </Link>}
+              <Link to="">
+                <a className="button is-primary">Edit</a>
+              </Link>
+              <Link to="">
+                <a className="button is-primary">Accept Invite</a>
+              </Link>
+              <Link to="">
+                <a className="button is-primary">Decline Invite</a>
+              </Link>
+            </div>
+          </footer>
+        </article>
       </div>
       <div className="actions">
-        {isPublic
-          ? ""
-          : <div>
+        {event.isPrivate &&
+          <div>
             <div className="attendees">
-              Attendees
+              Invited
             </div>
             <div className="attendees">
               Tasks
